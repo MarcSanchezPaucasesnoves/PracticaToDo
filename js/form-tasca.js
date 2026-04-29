e7c20a0f6c63fd3002e893184c39577e3783cf69
GitMarc <marcsanchez@paucasesnovescifp.cat>
2026-04-29 17:57:19 +0200

    feat(crear tasca): fer el js per crear noves tasques i guardarles al localStorage

---
+++ js/form-tasca.js
@@ -0,0 +1,7 @@
+import { crearLlistaTasques } from "./storage";
+
+document.addEventListener('DOMContentLoaded', () => {
+    crearLlistaTasques();
+
+    
+});
--- js/storage.js
+++ js/storage.js
@@ -32,3 +32,71 @@ export function eliminarCategoria(nom){
         }
     }
 }
+
+export function crearTasca(titol, descripcio, data, categoria, prioritat){
+    const tasques = JSON.parse(localStorage.getItem("llistaTasques"));
+
+    let tascaCreada = false;
+
+    if (comprovarCategoria(categoria) && comprovarTitolTasca(titol)) {
+
+        if (tasques.length == 0) {
+
+            const novaTasca = {
+                id: 1,
+                titol: titol,
+                descripcio: descripcio,
+                data: data,
+                categoria: categoria,
+                prioritat: prioritat,
+                realitzada: false
+            };
+            tasques.push(novaTasca);
+            localStorage.setItem("llistaTasques", JSON.stringify(tasques));
+            localStorage.setItem("idTask", 2);
+
+        } else {
+
+            const idTask = localStorage.getItem("idTask");
+            const novaTasca = {
+                id: idTask,
+                titol: titol,
+                descripcio: descripcio,
+                data: data,
+                categoria: categoria,
+                prioritat: prioritat,
+                realitzada: false
+            };
+
+            localStorage.setItem("idTask", idTask + 1);
+
+        }
+        tascaCreada = true;
+    }
+    return tascaCreada;
+    
+}
+
+export function comprovarCategoria(categoria){
+    const categories = JSON.parse(localStorage.getItem("llistaCategories"));
+    let existeix = false;
+
+    for (let index = 0; index < categories.length; index++) {
+        if (categories[index].nom === categoria) {
+            existeix = true;
+            break;
+        }
+    }
+    return existeix;
+
+}
+
+export function comprovarTitolTasca(titol){
+    return titol.trim().length > 0;
+}
+
+export function crearLlistaTasques(){
+    if (!localStorage.getItem("llistaTasques")) {
+        localStorage.setItem("llistaTasques", JSON.stringify([]));
+    }
+}
